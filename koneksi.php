dfaf7498f2ee73a722438549cf48205119dc4556
LAPTOP-UCMBO77B\ASUS <giysaputra28@gmail.com>
2023-06-09 17:47:36 +0700

    no message

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "jb.diamond";
+$host = "sql106.infinityfree.com";
+$username = "if0_34389746";
+$password = "IFhrfQA8dG";
+$database = "if0_34389746_yogi";
 
 $conect = mysqli_connect($host, $username, $password, $database);
 
